aaea4f9b147e24871f1334e4db4a047c11ad4b95
Harry E. Zhurov <harry.zhurov@gmail.com>
2016-04-05 19:07:50 +0600

    upgrade python utils script

--- samples/blackfin/gcc/bf533/6-round-robin/cfg/script/utils.py
+++ samples/blackfin/gcc/bf533/6-round-robin/cfg/script/utils.py
@@ -33,6 +33,13 @@ def pexec(cmd):
     return p.returncode, out, err
     
 #-------------------------------------------------------------------------------
+def sizeof_fmt(num, suffix='B'):
+    for unit in ['','Ki','Mi','Gi','Ti','Pi','Ei','Zi']:
+        if abs(num) < 1024.0:
+            return "%3.1f%s%s" % (num, unit, suffix)
+        num /= 1024.0
+    return "%.1f%s%s" % (num, 'Yi', suffix)
+#-------------------------------------------------------------------------------
 def print_size(datain):
     
     #--------------------------------------------
@@ -41,38 +48,54 @@ def print_size(datain):
     #
     res  = re.search('\.text\s+(\d+)\s+(\d+)\n',  datain)
     if res: csz = res.group(1)
-    else:   csz = 'none'
+    else:   csz = '0'
      
     res  = re.search('\.bss\s+(\d+)\s+(\d+)\n',  datain)
     if res: bsz= res.group(1)
-    else:   bsz = 'none'
+    else:   bsz = '0'
                
     res  = re.search('\.data\s+(\d+)\s+(\d+)\n',  datain)
     if res: dsz = res.group(1)
-    else:   dsz = 'none'
+    else:   dsz = '0'
     
-    res  = re.search('\.sdram\s+(\d+)\s+(\d+)\n',  datain)
-    if res: ssz = res.group(1)
-    else:   ssz = 'none'
+    res  = re.search('\.rodata\s+(\d+)\s+(\d+)\n',  datain)
+    if res: rosz = res.group(1)
+    else:   rosz = '0'
+
+    in_records = datain.split('\n')
+    sdram_size = 0;
+    for i in in_records:
+        res  = re.search('\.sdram\w*\s+(\d+)\s+(\d+)',  i)
+        if res: sdram_size += int(res.group(1))
+  
+    if sdram_size != 0:
+        ssz = sizeof_fmt(sdram_size)
+    else:
+        ssz = '0'
           
     #--------------------------------------------
     #
     #    Print results
     #
-    sep    = '+--------------+--------------+--------------+--------------+'
-    header = '|      code    |       bss    |      data    |     sdram    |'
+    sep    = '+--------------+--------------+--------------+--------------+--------------+'
+    header = '|      code    |       bss    |      data    |    rodata    |     sdram    |'
     
     fw    = 10                                         # field width
     
-    code  = '|' + ' '*(fw - len(csz)) + csz + '    '
-    bss   = '|' + ' '*(fw - len(bsz)) + bsz + '    '
-    data  = '|' + ' '*(fw - len(dsz)) + dsz + '    '
-    sdram = '|' + ' '*(fw - len(ssz)) + ssz + '    |'
+    code    = '|' + ' '*(fw - len(csz))  + csz  + '    '
+    bss     = '|' + ' '*(fw - len(bsz))  + bsz  + '    '
+    data    = '|' + ' '*(fw - len(dsz))  + dsz  + '    '
+    rodata  = '|' + ' '*(fw - len(rosz)) + rosz + '    '
+    sdram   = '|' + ' '*(fw - len(ssz))  + ssz  + '    |'
     
     print sep
     print header
     print sep
-    print code + bss + data + sdram
+    print code + bss + data + rodata + sdram
     print sep
+    
+    print '        L1 Code: ' + csz + ' bytes'
+    print '        L1 Data: ' + str( int(bsz) + int(dsz) + int(rosz) ) + ' bytes'
+    print ''
 #-------------------------------------------------------------------------------
 
